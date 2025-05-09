d25ffc1dac1eaf48bffb99604eea85bfeab06438
xieyuxuan666 <119400875+xieyuxuan666@users.noreply.github.com>
2025-05-09 16:22:05 +0800

    Update soushuba.py

--- soushuba.py
+++ soushuba.py
@@ -25,7 +25,7 @@ logger.addHandler(ch)
 
 def get_refresh_url(url: str):
     try:
-        response = requests.get(url)
+        response = requests.get(url, verify=False)
         if response.status_code != 403:
             response.raise_for_status()
 
